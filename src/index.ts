28aed78091dd9948a8d8a08f50b747398a6de9ca
nytocode <anto4te.dev@gmail.com>
2024-01-13 09:59:16 +0100

    setup for pug and tailwind

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
-import express from "express";
+import express, { Application } from "express";
+import path from "path";
+import dotenv from "dotenv";
 
-const app = express();
+dotenv.config();
+
+const app: Application = express();
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 const port = process.env.PORT ?? 3001;
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.render("home");
 });
 
 app.listen(3000, () => {
